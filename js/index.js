a7ea1d7bc147b4cbbd1ab4f6ef13d395c680e83b
Daniel Kazemian <Dkazem91@users.noreply.github.com>
2016-07-25 03:29:46 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
   $('input').delay(500).animate({
     width: "200px",
     borderTopLeftRadius: 20,
@@ -9,25 +8,25 @@ $(document).ready(function() {
     borderBottomRightRadius: 20
   }, 'slow');
 
-$('input').on('keydown', function(x) {
-if (x.which == '13') {
-  $(".input").css("margin-top", "0")
-  $(".random").fadeIn();
-  $(".data").hide();
-  $(".data").empty();
-  $.getJSON("https://en.wikipedia.org/w/api.php/w/api.php?action=query&format=json&prop=&list=search&meta=&pageids=&srsearch=" + $("input").val() + "&srprop=snippet&srlimit=10&callback=?", function(data) {
-    $.each(data.query.search, function(index, value) {
-      var url = "http://en.wikipedia.org/wiki/" + value.title.replace(/ /g, "_");
-      var entry = $("<a class='results' href=" + url + "><div>" + "<P><strong>" + value.title + "</strong></p>" + "<p>" + value.snippet + "</p></div></a>");
-      $(".data").append(entry);
+  $('input').on('keydown', function(x) {
+    if (x.which == '13') {
+      $(".input").css("margin-top", "0")
+      $(".random").fadeIn();
+      $(".data").hide();
+      $(".data").empty();
+      $.getJSON("https://en.wikipedia.org/w/api.php/w/api.php?action=query&format=json&prop=&list=search&meta=&pageids=&srsearch=" + $("input").val() + "&srprop=snippet&srlimit=10&callback=?", function(data) {
+        $.each(data.query.search, function(index, value) {
+          var url = "http://en.wikipedia.org/wiki/" + value.title.replace(/ /g, "_");
+          var entry = $("<a class='results' href=" + url + "><div>" + "<P><strong>" + value.title + "</strong></p>" + "<p>" + value.snippet + "</p></div></a>");
+          $(".data").append(entry);
 
-    })
+        })
 
-  })
-  setTimeout(function() {
-    $(".data").fadeIn();
-  }, 500);
-}
-});
-  
+      })
+      setTimeout(function() {
+        $(".data").fadeIn();
+      }, 500);
+    }
+  });
+
 });
